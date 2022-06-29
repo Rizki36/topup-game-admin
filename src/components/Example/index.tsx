3548da446bad41b5a1edffb44fef727db17527de
Rizki fitra rahman <39044004+Rizki36@users.noreply.github.com>
2022-06-29 23:08:06 +0700

    feat: init storybook

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import style from './index.module.scss';
+
+const Example: FC = () => {
+  return <button className={style.example}>Enter</button>;
+};
+
+export default Example;
